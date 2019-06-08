8ef6aabc7fd1ffa50ec7dee6366f108fceaef5bb
ernesto <ernestoballono@gmail.com>
2019-06-08 13:49:45 -0400

    Updates

---
+++ precache-manifest.9e117e11bd70eb66e976e24bd69f513c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fe72bd143127e184dd2621bb390c7f",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "ae46c2c9f47d3d93d23f",
+    "url": "/myapp/static/css/2.0eabb4e8.chunk.css"
+  },
+  {
+    "revision": "3106851f8545624991e9",
+    "url": "/myapp/static/css/main.65c6349d.chunk.css"
+  },
+  {
+    "revision": "ae46c2c9f47d3d93d23f",
+    "url": "/myapp/static/js/2.ee42897c.chunk.js"
+  },
+  {
+    "revision": "3106851f8545624991e9",
+    "url": "/myapp/static/js/main.96fa2837.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "2dd6b4ff1b41eddcf528cfe1e6994ffe",
+    "url": "/myapp/static/media/logo.2dd6b4ff.webp"
+  }
+]);
